521e4279f0cad1636a73b49bff96f8231f3aba04
rhiannonrobson <rhiannonrobson8@gmail.com>
2024-12-06 00:14:35 +0400

    final final edit

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-/* Pseudocode for Quizgame
-1. Set up game home page - complete
-2.Set up game rules to show when you click on Gmae rules buttom -complete
-3.link question info
-4.link answers to boxes
-5. Set up clicker
-6.link level info
-7.link lives info
-8.Set up loop for game play
-    - page loads question
-    - player clicks anser
-    - if correct move to next question
-    - if incorrect loose life and move to next quetion
-    - if you loose all your lives you loose the game
-    - if you reach level 10 with all of your lives you win
-*/
 /*-------------------------------- Constants --------------------------------*/
 
 let boxIndex;
